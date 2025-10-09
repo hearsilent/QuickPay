26e31b38ccf23dc45baaca3e37b83601a45df270
HearSilent <hearsilent@users.noreply.github.com>
2025-10-09 20:44:29 +0800

    <feat> support PxPayPlus

---
+++ app/src/main/java/com/hearsilent/quickpay/PxPayPlusTileService.kt
@@ -0,0 +1,49 @@
+package com.hearsilent.quickpay
+
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
+import android.service.quicksettings.Tile.STATE_INACTIVE
+import android.service.quicksettings.TileService
+import androidx.core.net.toUri
+
+class PxPayPlusTileService : TileService() {
+
+    override fun onStartListening() {
+        super.onStartListening()
+        qsTile.state = STATE_INACTIVE
+        qsTile.updateTile()
+    }
+
+    @SuppressLint("StartActivityAndCollapseDeprecated")
+    override fun onClick() {
+        super.onClick()
+        val packageName = "com.pxpayplus.app"
+        try {
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = "com.pxpay.plus://zjdja/...".toUri()
+                setClassName(packageName, "com.pxpayplus.sdk.DeepLinkRecognizeActivity")
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                startActivityAndCollapse(
+                    PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+                )
+            } else {
+                startActivityAndCollapse(intent)
+            }
+        } catch (_: Exception) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.data = "market://details?id=$packageName".toUri()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                startActivityAndCollapse(
+                    PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+                )
+            } else {
+                startActivityAndCollapse(intent)
+            }
+        }
+    }
+}
